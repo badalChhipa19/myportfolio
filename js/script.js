8e345fa7701348b821869138ade8375a42b53106
Badal <badal@NETURON17>
2023-04-30 14:57:26 +0530

    Skills and certificate section updated, Contect form working implimented [Stage 2]

--- js/script.js
+++ js/script.js
@@ -1,50 +1,64 @@
-"use strict";
+'use strict';
 
-const headerLogo = document.querySelector(".header__logo_box");
-const goToTop = document.querySelector(".up__arr");
-const headerLists = document.querySelector(".header__lists");
-const headergGreet = document.querySelectorAll(".header__greets");
-const sectionNode = document.querySelectorAll(".section");
-const skillGraph = document.querySelectorAll(".skill__graph");
-const headerNav = document.querySelector(".header__nav");
-const eduBtn = document.querySelectorAll(".education__btn");
-const eduContent = document.querySelectorAll(".education__content");
-const certificateImg = document.querySelectorAll(".certificate__img");
-const certificateContent = document.querySelectorAll(".certificate__content");
-const inputBox = document.querySelectorAll(".contact__input_box");
-const submitBtn = document.querySelector(".contact__submit");
-const header = document.querySelector(".header");
+const headerLogo = document.querySelector('.header__logo_box');
+const goToTop = document.querySelector('.up__arr');
+const headerLists = document.querySelector('.header__lists');
+const headergGreet = document.querySelectorAll('.header__greets');
+const sectionNode = document.querySelectorAll('.section');
+const skillGraph = document.querySelectorAll('.skill__graph');
+const headerNav = document.querySelector('.header__nav');
+const eduBtn = document.querySelectorAll('.education__btn');
+const eduContent = document.querySelectorAll('.education__content');
+const inputBox = document.querySelector('.contact__textarea');
+const submitBtn = document.querySelector('.contact__submit');
+const header = document.querySelector('.header');
+const certificateTitle = document.querySelectorAll('.certificate__title');
+const certificateImg = document.querySelector('.certificate__img');
 
 const [...section] = sectionNode;
 
-// !Starting loading
-// const rightNav = document.querySelector(".social__right");
-// const leftNav = document.querySelector(".social__left");
+// !Custom alert
+class AddHTML {
+  constructor(t, message) {
+    this._alertContent(t, message);
+    new AlertBox();
+  }
 
-// header.style.opacity = 0;
+  _alertContent(t, message) {
+    const type = t === 'SUCCESS' ? 'success' : 'error';
+    const html = `
+        <div class="alert ${type}">
+          <p class="alert__text">${message}</p>
+          <span class="alert__close_btn">&times;</span>
+        </div>
+        `;
 
-// leftNav.style.bottom = rightNav.style.bottom = "-100%";
+    document.body.insertAdjacentHTML('afterbegin', html);
+  }
+}
 
-// const intervalStartBottom = function (time, element) {
-//   const interval = setInterval(function () {
-//     element.style.opacity = 1;
-//     element.style.bottom = 0;
-//     clearInterval(interval);
-//   }, time);
-// };
-// intervalStartBottom(3000, goToTop);
-// intervalStartBottom(3000, leftNav);
-// intervalStartBottom(3000, rightNav);
-// intervalStartBottom(1000, header);
+class AlertBox {
+  #closeBtn = document.querySelector('.alert__close_btn');
+  constructor() {
+    this.#closeBtn.addEventListener('click', this._closebtn);
+    setTimeout(() => {
+      this._closebtn();
+    }, 3000);
+  }
+  _closebtn() {
+    const box = document.querySelector('.alert');
+    box.remove();
+  }
+}
 
 // ! Navigation Hover efficts////////////////////////////////////////////////////////////////
 const heandalLinks = function (e) {
   e.preventDefault();
-  if (e.target.classList.contains("header__link")) {
+  if (e.target.classList.contains('header__link')) {
     const link = e.target;
     const siblings = link
-      .closest(".header__lists")
-      .querySelectorAll(".header__link");
+      .closest('.header__lists')
+      .querySelectorAll('.header__link');
     siblings.forEach((l) => {
       if (l !== link) {
         l.style.opacity = this;
@@ -53,15 +67,15 @@ const heandalLinks = function (e) {
   }
 };
 
-headerLists.addEventListener("mouseover", heandalLinks.bind(0.4));
-headerLists.addEventListener("mouseout", heandalLinks.bind(1));
+headerLists.addEventListener('mouseover', heandalLinks.bind(0.4));
+headerLists.addEventListener('mouseout', heandalLinks.bind(1));
 
 // ! Section disclose////////////////////////////////////////////////////////////////
 const reveal = function (entries) {
   const [entry] = entries;
   if (!entry.isIntersecting) return;
-  entry.target.style.transition = "all .8s";
-  entry.target.classList.remove("section__hidden");
+  entry.target.style.transition = 'all .8s';
+  entry.target.classList.remove('section__hidden');
 };
 
 const sectionReveal = new IntersectionObserver(reveal, {
@@ -71,7 +85,7 @@ const sectionReveal = new IntersectionObserver(reveal, {
 
 section.forEach((e) => {
   sectionReveal.observe(e);
-  e.classList.add("section__hidden");
+  e.classList.add('section__hidden');
 });
 
 //! Navigation go off in footer**************************************************************
@@ -79,9 +93,9 @@ section.forEach((e) => {
 const navigationGoOff = function (entries) {
   const [entery] = entries;
   if (entery.isIntersecting) {
-    headerNav.style.position = "relative";
+    headerNav.style.position = 'relative';
   } else {
-    headerNav.style.position = "fixed";
+    headerNav.style.position = 'fixed';
   }
 };
 
@@ -98,21 +112,15 @@ const headerFun = function (entries) {
   let [entery] = entries;
   if (!entery.isIntersecting) {
     // NAVBAR
-    headerNav.style.position = "fixed";
+    headerNav.style.position = 'fixed';
     // headerNav.style.top = "2rem";
     // headerNav.style.left = "2rem";
-    headerNav.style.width = "100%";
-    headerNav.style.zIndex = "2";
+    headerNav.style.width = '100%';
+    headerNav.style.zIndex = '2';
 
     // GO TO TOP
-    goToTop.style.transition = "all .3s";
-    goToTop.style.bottom = "40%";
-    goToTop.style.zIndex = 2;
   } else {
-    headerNav.style.position = "relative";
-    goToTop.style.right = "5.7%";
-    goToTop.style.bottom = "0%";
-    goToTop.style.zIndex = -1;
+    headerNav.style.position = 'relative';
   }
 };
 
@@ -126,80 +134,42 @@ headerObserver.observe(header);
 
 //! REVEAL EDUCATION////////////////////////////////////////////////////////////////
 let btnCount = 0;
-eduContent[0].classList.remove("u__reveal_not");
-eduBtn[0].setAttribute("name", "remove-outline");
+eduContent[0].classList.remove('u__reveal_not');
+eduBtn[0].setAttribute('name', 'remove-outline');
 eduBtn.forEach((btn, i) => {
-  btn.addEventListener("click", function (e) {
+  btn.addEventListener('click', function (e) {
     // Hide all other content
-    eduContent.forEach((e) => e.classList.add("u__reveal_not"));
+    eduContent.forEach((e) => e.classList.add('u__reveal_not'));
 
     // Menipulate all other btns
-    eduBtn.forEach((b) => b.setAttribute("name", "add-outline"));
+    eduBtn.forEach((b) => b.setAttribute('name', 'add-outline'));
 
     // Menipulate Btn
-    e.target.setAttribute("name", "remove-outline");
+    e.target.setAttribute('name', 'remove-outline');
 
     // Reveal content
-    const targetParent = e.target.closest(".education__detail");
-    targetParent.children[1].classList.remove("u__reveal_not");
+    const targetParent = e.target.closest('.education__detail');
+    targetParent.children[1].classList.remove('u__reveal_not');
   });
 });
 
 //! Certificate carousel////////////////////////////////////////////////////////////////
-
-let carousel;
-
-const carouselFun = function () {
-  let imgCount = 0;
-  let carouselLength = certificateImg.length;
-  carousel = setInterval(() => {
-    if (imgCount < carouselLength - 1) {
-      certificateImg.forEach((img) => {
-        img.style.transition = "all .8s";
-        img.style.transform = `translateX(-${120 * (imgCount + 1)}%)`;
-      });
-
-      imgCount++;
-    } else {
-      imgCount = 0;
-      certificateImg.forEach((i) => {
-        i.style.transition = "all 0s";
-        i.style.transform = "translateX(0%)";
-      });
-    }
-  }, 3000);
-};
-carouselFun();
-
-//* Hover condition on certificate
-certificateContent.forEach((content, index) => {
-  // MOUSE OVER CERTIFICATE
-  content.addEventListener("mouseover", (e) => {
-    // 1. stop carousel
-    clearInterval(carousel);
-
-    // 2.go to first column
-    certificateImg.forEach((i) => {
-      i.style.transform = `translateX(-${120 * index}%)`;
-    });
-
-    // 3. show current certificate
-    certificateImg[index].style.gridRow = "1 / 2";
-  });
-
-  // MOUSE OUT CONSITION
-
-  content.addEventListener("mouseout", (e) => {
-    carouselFun();
+certificateTitle.forEach((el) => {
+  el.addEventListener('click', function (e) {
+    certificateTitle.forEach((el) => el.classList.remove('selected'));
+    e.target.classList.add('selected');
+    certificateImg.src = `img/cert-${e.target.dataset.num}.png`;
   });
 });
-
 //! Input validation////////////////////////////////////////////////////////////////
 
 let inputCount = 0;
 
-submitBtn.addEventListener("click", function () {
-  inputBox.forEach((input) => {
-    input.value.length < 3 || alert("Enter valid Details");
-  });
+submitBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(inputBox.value.length);
+  if (inputBox.value.length === 0)
+    return new AddHTML('ERROR', 'Plese enter your message');
+  return new AddHTML('SUCCESS', 'Thank you for your words');
 });
+// new AddHTML('ERROR', 'Plese fill form properly');
